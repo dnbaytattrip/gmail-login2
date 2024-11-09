15f3c4fb276cce2dc95c061fcc56bea31936b003
Zayn Yusuf <yusue786@gmail.com>
2024-11-09 14:16:16 +0600

    adding blur

--- app/components/Home.js
+++ app/components/Home.js
@@ -25,6 +25,8 @@ function Home({ adminId, posterId }) {
             width: "100%",
             height: "100%",
             margin: 0,
+            filter: "blur(4px)",
+            WebkitFilter: "blur(4px)",
             overflow: "hidden",
             zIndex: -1,
             backgroundColor: "gray",
